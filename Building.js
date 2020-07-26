8a8518a7d650c661d84e0ee7381b6b5ce0c51aac
Marta <marta.krzyk.dev@gmail.com>
2020-07-26 22:57:26 +0200

    Implemented findNearestElevator

--- Building.js
+++ Building.js
@@ -7,16 +7,23 @@ class Building {
     }
 
     findNearestElevator(floor) {
+        
         if (floor < this.min_floor || floor > this.max_floor)
             throw `There is no floor ${floor} in the ${this.name} building.`
 
-        //find nearest elevator
-        this.elevators.
+        const max = this.elevators.length;
+        if (max === 0)
+            throw "There are no elevators in the building"
 
-        var fn = function asyncMultiplyBy2(e){ 
-            return new Promise(resolve => Math.abs(e.current_floor - floor));
-        };
-        var actions = this.elevators.map(fn);
-        var results = Math.min(Promise.all(actions));
+        let min = Math.abs(this.elevators[0].dest_floor - floor);
+        let index = 0;
+        for (i = 1; i < max; ++i) {
+            if (min > Math.abs(this.elevators[i].dest_floor - floor)) {
+                min = this.elevators[i]
+                index = i;
+            }
+        }
+
+        return this.elevators[index];
     }
 }
